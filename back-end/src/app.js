ff7e32d95120d554829a2c2d8b2331efbd207bbb
maria lucato <maria.lucato@etec.sp.gov.br>
2025-09-18 21:36:08 -0300

    a

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -7,10 +7,4 @@ app.use(express.json());
 // Registrar rotas
 app.use(routes);
 
-// Subir servidor
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-});
-
 export default app;
