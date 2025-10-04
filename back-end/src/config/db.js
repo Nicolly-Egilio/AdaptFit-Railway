f0ba82b8190ea36ec041f546011d5aed67b2b8e3
Nicolly-Egilio <nicollysegilio@outlook.com>
2025-10-04 15:07:17 -0300

    preparando para o deploy

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -1,18 +1,30 @@
+// import mysql from "mysql2";
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",       
+//   password: "",       
+//   database: "Adapt-Fit" 
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error("Erro ao conectar ao MySQL:", err);
+//     return;
+//   }
+//   console.log("Conectado ao MySQL!");
+// });
+
 import mysql from "mysql2";
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",       
-  password: "",       
-  database: "Adapt-Fit" 
-});
+const connection = mysql.createConnection(process.env.MYSQL_URL);
 
 connection.connect((err) => {
   if (err) {
     console.error("Erro ao conectar ao MySQL:", err);
-    return;
+  } else {
+    console.log("Conectado ao MySQL!");
   }
-  console.log("Conectado ao MySQL!");
 });
 
 export default connection;
